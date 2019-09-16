bd2684e95ac0da02d5b5017280cb96aede033d83
Francis Gonzalez <Kitomc.rd@gmail.com>
2019-09-16 03:37:09 -0400

    cree mis 3 formularios

---
+++ Formulario2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaGitHub_FrancisAlexander
+{
+    public partial class Formulario2 : UserControl
+    {
+        public Formulario2()
+        {
+            InitializeComponent();
+        }
+    }
+}
