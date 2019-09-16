b93325bb065c9de48388936c1c796ef5c75b50c9
jefferson <jeison37@hotmail.es>
2019-09-16 03:39:17 -0400

    puse mi nombre

--- Formulario2.Designer.cs
+++ Formulario2.Designer.cs
@@ -28,18 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Formulario2
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Name = "Formulario2";
-            this.Size = new System.Drawing.Size(668, 436);
-            this.ResumeLayout(false);
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(463, 137);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "jefferson";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// Formulario2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.button1);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "Formulario2";
+			this.Size = new System.Drawing.Size(891, 537);
+			this.ResumeLayout(false);
 
         }
 
-        #endregion
-    }
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+	}
 }
