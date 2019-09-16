302c26d55c94e886ca60d58198e41194bb52f7b6
Francis Gonzalez <Kitomc.rd@gmail.com>
2019-09-16 03:08:58 -0400

    Agregue diseño y cerrado

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,43 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.label1 = new System.Windows.Forms.Label();
-			this.button1 = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(253, 154);
-			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(310, 42);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Francis Gonzalez";
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(407, 292);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(912, 485);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.label1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.pnlIzquierda = new System.Windows.Forms.Panel();
+            this.pnlContenedor = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnlHeader.SuspendLayout();
+            this.pnlContenedor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(139, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Francis Gonzalez";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 179);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pnlHeader.Controls.Add(this.label2);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(684, 35);
+            this.pnlHeader.TabIndex = 2;
+            // 
+            // pnlIzquierda
+            // 
+            this.pnlIzquierda.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pnlIzquierda.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlIzquierda.Location = new System.Drawing.Point(0, 35);
+            this.pnlIzquierda.Name = "pnlIzquierda";
+            this.pnlIzquierda.Size = new System.Drawing.Size(136, 359);
+            this.pnlIzquierda.TabIndex = 3;
+            // 
+            // pnlContenedor
+            // 
+            this.pnlContenedor.Controls.Add(this.button1);
+            this.pnlContenedor.Controls.Add(this.label1);
+            this.pnlContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContenedor.Location = new System.Drawing.Point(136, 35);
+            this.pnlContenedor.Name = "pnlContenedor";
+            this.pnlContenedor.Size = new System.Drawing.Size(548, 359);
+            this.pnlContenedor.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(625, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 394);
+            this.Controls.Add(this.pnlContenedor);
+            this.Controls.Add(this.pnlIzquierda);
+            this.Controls.Add(this.pnlHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.pnlContenedor.ResumeLayout(false);
+            this.pnlContenedor.PerformLayout();
+            this.ResumeLayout(false);
 
         }
 
@@ -72,6 +123,10 @@
 
         private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button button1;
-	}
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel pnlIzquierda;
+        private System.Windows.Forms.Panel pnlContenedor;
+    }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace PruebaGitHub_FrancisAlexander
         {
             InitializeComponent();
         }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
